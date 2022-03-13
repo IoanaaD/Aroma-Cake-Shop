dfaf72d542a80aebf9414f15c601b10c6fbb331d
Ioana Dumitrescu <Ioana.Dumitrescu@ibm.com>
2022-03-13 16:50:46 +0200

    sample data preparation

---
+++ backend/data/users.js
@@ -0,0 +1,27 @@
+import bcrypt from 'bcryptjs'
+
+const user = [
+    {
+        name: 'Admin user',
+        email: 'admin@example.com',
+        password: bcrypt.hashSync('123456', 10),
+        isAdmin: true
+    },
+    {
+        name: 'Maria Popescu',
+        email: 'maria@example.com',
+        password: bcrypt.hashSync('123456', 10),
+    },
+    {
+        name: 'Andreea Vasile',
+        email: 'andreea@example.com',
+        password: bcrypt.hashSync('123456', 10),
+    },
+    {
+        name: 'Mircea Tanase',
+        email: 'mircea@example.com',
+        password: bcrypt.hashSync('123456', 10),
+    }
+]
+
+export default users
